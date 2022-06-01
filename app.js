e99dd5414d82f5f71b0fde8ddec7cc0724a8101c
Safar123 <azajsbd111@gmail.com>
2022-06-01 10:32:44 +0545

    route directory for tour

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
-
 const app = express();
+
 app.use(express.json());
 
+//routes declaration
+const tourRoute = require('./routes/tourRoutes');
 
+
+
+//route initialization
+app.use('/api/v1/tours', tourRoute )
 
 module.exports = app;
