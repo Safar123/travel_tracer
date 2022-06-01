c1fd07c3bcb6145d71d5d36d7f53a884816acca7
Safar123 <azajsbd111@gmail.com>
2022-06-01 12:52:12 +0545

    implementing user route

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ app.use(express.json());
 
 //routes declaration
 const tourRoute = require('./routes/tourRoutes');
+const userRoute = require('./routes/userRoutes');
 
 
 
 //route initialization
-app.use('/api/v1/tours', tourRoute )
+app.use('/api/v1/tours', tourRoute );
+app.use('/api/v1/users', userRoute);
 
 module.exports = app;
