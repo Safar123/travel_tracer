583ae735d98ee5f805f07164c5c58ab0e13696f0
Safar123 <azajsbd111@gmail.com>
2022-06-02 09:24:43 +0545

    authentication routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { signUpUser } = require('../controller/authController');
-const { getAllUser } = require('../controller/userController');
+const { getAllUser, removeUser } = require('../controller/userController');
 
 const router = express.Router();
 
 router.route('/').get(getAllUser);
+router.route('/:id').delete(removeUser)
 router.route('/signup').post(signUpUser);
 
 module.exports = router;
